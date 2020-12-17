17ab49213de9017215e813b105018e6c84e9e9a7
firmanputra908 <61071496+firmanputra908@users.noreply.github.com>
2020-12-17 22:54:15 +0700

    Add files via upload

---
+++ hitung1-100.py
@@ -0,0 +1,12 @@
+#definisikan variabel
+a= 1
+
+#lakukan looping dengan while
+while a < 100:  
+    print(a)
+    a = a+3
+
+# a=int(input('nilai 1:'))
+# b=int(input('nilai 2:'))
+# total=a+b
+# print(total)
